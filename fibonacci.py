3f5063e876a6ef8b126354b1a1b535fb655715eb
Cangur <112434361+cangurae@users.noreply.github.com>
2026-05-10 00:03:21 +0200

    Fix variable name from 'suma' to 'sum'

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
 num1 = int(0)
 num2 = int(1)
-suma = int(num1 + num2)
+sum = int(num1 + num2)
 while sum < 10000:
-    print(f"{suma}")
+    print(f"{sum}")
     sum = sum + num2
     num2 = sum - num2
 
